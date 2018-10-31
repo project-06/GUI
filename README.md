6b80cea3afcc7b617ab27bf7a6eadf1c11566b74
Tayyip Ensar Özkaya <42948152+tensarflow@users.noreply.github.com>
2018-10-31 20:36:22 +0300

    Create Project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GUI
 Graphical User Interface of the Monitoring System
+
+# For readCSV
+
+1. Copy data.csv to any path.
+2. Open readCSV.sln in GUI/readCSV
+3. In the solution explorer, Select Form1.cs > Form1.Designer.cs > Form1 and open Form1()
+4. Copy the path of data.csv to line 23 between the " ".
+5. Run and click the button to see the values in the "Output" window of Visual Studio
---
+++ readCSV/readCSV/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace readCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            using (var reader = new StreamReader(@"C:\data.csv"))
+            {
+                List<string> stream = new List<string>();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    Console.Out.WriteLine(values[0]);
+                }
+            }
+        }
+    }
+}
