8cd99369f6bf81d38315b77ec53f24712ac9cf1d
Josephkhan96 <sam@example.com>
2018-11-01 20:53:54 +0200

    Yeni versiyon

--- readCSV/readCSV/Form1.cs
+++ readCSV/readCSV/Form1.cs
@@ -8,28 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace readCSV
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            
         }
         
         private void button1_MouseClick(object sender, MouseEventArgs e)
         {
-            using (var reader = new StreamReader(@"C:\data.csv"))
+            
+            using (var reader = new StreamReader(@"C:\Users\Asus\Desktop\data.csv"))
             {
                 List<string> stream = new List<string>();
 
-                while (!reader.EndOfStream)
-                {
+                //while (!reader.EndOfStream)
+                //{
                     var line = reader.ReadLine();
                     var values = line.Split(';');
+                    
                     Console.Out.WriteLine(values[0]);
-                }
+                   
+                //}
+
+                this.chart1.Series["Series1"].Points.AddY(values[0]);
             }
         }
     }
