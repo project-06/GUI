6f5ce0e393eb90ed8b6f3a0849250cc3c022c814
Josephkhan96 <sam@example.com>
2018-12-15 16:48:26 +0300

    Animasyonlu

--- Demo/MultipleSensorRealTimeMonitoring/MultipleSensorRealTimeMonitoring/Form1.Designer.cs
+++ Demo/MultipleSensorRealTimeMonitoring/MultipleSensorRealTimeMonitoring/Form1.Designer.cs
@@ -28,85 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(804, 361);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chart1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(796, 335);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Leistungsanalyse";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // chart1
             // 
             this.chart1.BackColor = System.Drawing.Color.DimGray;
             this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(12, 12);
+            chartArea1.AxisX2.Title = "SecondaryXaxis";
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(8, 6);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(625, 212);
-            this.chart1.TabIndex = 0;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(765, 274);
+            this.chart1.TabIndex = 2;
             this.chart1.Text = "Daten";
-            title2.Name = "Sensor1";
-            this.chart1.Titles.Add(title2);
+            title1.Name = "Sensor1";
+            this.chart1.Titles.Add(title1);
             // 
-            // button1
+            // tabPage2
             // 
-            this.button1.Location = new System.Drawing.Point(713, 306);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Controls.Add(this.pictureBox3);
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(796, 335);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Status";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // richTextBox1
+            // panel1
             // 
-            this.richTextBox1.Enabled = false;
-            this.richTextBox1.Location = new System.Drawing.Point(12, 246);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(625, 96);
-            this.richTextBox1.TabIndex = 4;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.panel1.BackColor = System.Drawing.Color.LimeGreen;
+            this.panel1.Location = new System.Drawing.Point(50, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(708, 22);
+            this.panel1.TabIndex = 3;
             // 
-            // openFile
+            // pictureBox3
             // 
-            this.openFile.FileName = "openFile";
-            this.openFile.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            this.pictureBox3.Image = global::MultipleSensorRealTimeMonitoring.Properties.Resources.giphybox;
+            this.pictureBox3.Location = new System.Drawing.Point(626, 159);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 56);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MultipleSensorRealTimeMonitoring.Properties.Resources.giphydrucker;
+            this.pictureBox2.Location = new System.Drawing.Point(337, 57);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(187, 158);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MultipleSensorRealTimeMonitoring.Properties.Resources.robotarm;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Status:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Demo/MultipleSensorRealTimeMonitoring/MultipleSensorRealTimeMonitoring/Form1.cs
+++ Demo/MultipleSensorRealTimeMonitoring/MultipleSensorRealTimeMonitoring/Form1.cs
@@ -7,58 +7,97 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
+using System.IO;
 using rtChart;
 using System.Diagnostics;
 using System.Threading;
 using System.IO;
+//using NetMQ.Sockets;
+//using NetMQ;
+using System.Windows.Forms.DataVisualization.Charting;
 
-//KODUN DÜZGÜN ÇALIŞABİLMESİ İÇİN ÇÖZÜMGEZGİNİ>BAŞVURULAR>SAĞCLICK>NUGETPAKETLERİNİYÖNET 
-//YAPTIKTAN SONRA "kayChart" adlı nuggeti ekleyin. Program düzgün çalışacaktır.
+
+
 
 namespace MultipleSensorRealTimeMonitoring
 {
     public partial class Form1 : Form
     {
 
-        
+        //Robot robot = new Robot();
+        //ResponseSocket server = null;
+        int eventID = 0;
+        float[] data;
 
         public Form1()
         {
             InitializeComponent();
-        }
-
-
-       
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-            try
-            {
-                openFile.Filter = "Textfile(*txt) |*.txt";
-                openFile.ShowDialog();   
-                StreamReader Reading = new StreamReader(openFile.FileName);
-                richTextBox1.Text = Reading.ReadToEnd();
-                Reading.Close();
-            }
-            catch(Exception hata)
-            {
-                MessageBox.Show(hata.Message);
-            }
 
         }
+
 
         PerformanceCounter Data = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
 
-        //RASTGELE VERILER BU FONKSIYON ILE SAĞLANIYOR.
+        
         private double randomzahlen()
         {
             Random zahlen = new Random();
-            return zahlen.Next(0, 1000);
+            return zahlen.Next(0, 500);
         }
+
+        //private void receiveData()
+        //{
+            //ActualCurrent actualCurrent = new ActualCurrent();
+            //DataProcessor dataProcessor = new DataProcessor();
+
+        //    while (true)
+        //    {
+        //        Receive data through socket and send answer
+        //        string message = server.ReceiveFrameString();
+        //        server.SendFrame("Roger");
+
+        //        Adjust string and convert to float and create new eventobject
+        //        data = dataProcessor.String2float(message);
+
+        //        Look for changes, print message if there is a difference; initialze for next loop
+        //        for (int i = 0; i < 6; i++)
+        //            {
+        //                float TOL = .5F;       //Tolerance
+        //                if (!((data[i] > actualCurrent.amperetData[i] - TOL) && (data[i] < actualCurrent.amperetData[i] + TOL)))
+        //                {
+        //                    string logMessage = "Something is happening -> Event ID: " + eventID;
+        //                    panel1.BackColor = Color.Lime;
+        //                    eventID++;
+        //                    updateLogFile(logMessage);
+             
+
+        //                    break;
+        //                }
+        //                else
+        //                    panel1.BackColor = Color.DarkGray;
+        //            }
+        //        actualCurrent.amperetData = data;
+        //    }
+        //}
+
+        //delegate void SetTextCallback(string text);
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+           
+
+            StripLine stripLine1 = new StripLine();
+            stripLine1.StripWidth = 15;       //BİTİŞ YERİ(Daha doğrusu genişliği)
+            stripLine1.BorderColor = System.Drawing.Color.Red;
+            stripLine1.BorderWidth = 10;
+            stripLine1.BorderDashStyle = ChartDashStyle.Solid;
+            stripLine1.IntervalOffset = 40;   //bAŞLANGIÇ YERİ
+            stripLine1.BackColor = System.Drawing.Color.WhiteSmoke;
+            chart1.ChartAreas[0].AxisX.StripLines.Add(stripLine1);
+
+
             kayChart SensorData1 = new kayChart(chart1, 60);
             SensorData1.serieName = "Series1";
 
@@ -72,14 +111,157 @@ namespace MultipleSensorRealTimeMonitoring
 
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-            richTextBox1.Invoke((MethodInvoker)delegate { richTextBox1.AppendText("Deger: " + randomzahlen() + "\n"); });
-        }
+//        private void readLogFile(string a)
+//        {
+//            try
+//            {
+//                FileStream fileStream = new FileStream("C:/Users/teozk/OneDrive/Dokumente/Log.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
 
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
+//                StreamReader streamReader = new StreamReader(fileStream);
 
-        }
-    }
+//                if (this.textBox1.InvokeRequired)
+//                {
+//                    SetTextCallback d = new SetTextCallback(readLogFile);
+//                    this.Invoke(d, new object[] { streamReader.ReadToEnd() });
+//                }
+//                else
+//                {
+//                    this.textBox1.Text = streamReader.ReadToEnd();
+//                }
+
+
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine(e.Message);
+//            }
+//        }
+
+//        public void updateLogFile(string logMessage, bool isEvent = true)
+//        {
+//            string Text;
+//            if (isEvent == false)
+//                Text = logMessage;
+//            else
+//                Text = "\r\nLog Entry : " + DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture) + " | " + DateTime.Now.ToLongDateString() + " -------------------- " + logMessage;
+
+//            string Path = "C:/Users/teozk/OneDrive/Dokumente/Log.txt";
+
+//            string content = File.ReadAllText(Path);
+//            content = Text + "\n" + content;
+//            File.WriteAllText(Path, content);
+//            readLogFile(" ");
+//        }
+
+//        private void button1_Click(object sender, EventArgs e)
+//        {
+//            // Initialize socket communication with robot
+//            updateLogFile("\r\nBinding socket... Please start the server.", false);
+//            server = new ResponseSocket();
+//            server.Bind("tcp://localhost:5555");
+
+//            Thread receiveDataThread = new Thread(receiveData);
+//            receiveDataThread.Start();
+//        }
+
+//        private void button2_Click(object sender, EventArgs e)
+//        {
+//            updateLogFile("\r\nInitializing Robot...", false);
+//            robot.initialize("169.254.156.200");
+//            updateLogFile("\r\nInitializing Done.", false);
+//        }
+
+//        private void button3_Click(object sender, EventArgs e)
+//        {
+//            updateLogFile("\r\nMoving Robot Sample Path..", false);
+//            Thread robotMovementThread = new Thread(robot.moveSample);
+//            robotMovementThread.Start();
+//        }
+
+//        private void button4_Click(object sender, EventArgs e)
+//        {
+//            // Plot Data !!!
+//            kayChart sensor1 = new kayChart(chart1, 20);
+//            kayChart sensor2 = new kayChart(chart2, 20);
+//            kayChart sensor3 = new kayChart(chart3, 20);
+//            kayChart sensor4 = new kayChart(chart4, 20);
+//            kayChart sensor5 = new kayChart(chart5, 20);
+//            kayChart sensor6 = new kayChart(chart6, 20);
+
+//            sensor1.serieName = "Series1";
+//            sensor2.serieName = "Series1";
+//            sensor3.serieName = "Series1";
+//            sensor4.serieName = "Series1";
+//            sensor5.serieName = "Series1";
+//            sensor6.serieName = "Series1";
+
+//            Task.Factory.StartNew(() =>
+//            {
+//                sensor1.updateChart(updateSensor1, 400);
+//            });
+
+//            Task.Factory.StartNew(() =>
+//            {
+//                sensor2.updateChart(updateSensor2, 400);
+//            });
+
+//            Task.Factory.StartNew(() =>
+//            {
+//                sensor3.updateChart(updateSensor3, 400);
+//            });
+
+//            Task.Factory.StartNew(() =>
+//            {
+//                sensor4.updateChart(updateSensor4, 400);
+//            });
+
+//            Task.Factory.StartNew(() =>
+//            {
+//                sensor5.updateChart(updateSensor5, 400);
+//            });
+
+//            Task.Factory.StartNew(() =>
+//            {
+//                sensor6.updateChart(updateSensor6, 400);
+//            });
+//        }
+
+//        private double updateSensor1()
+//        {
+//            return data[0];
+//        }
+
+//        private double updateSensor2()
+//        {
+//            return data[1];
+//        }
+
+//        private double updateSensor3()
+//        {
+//            return data[2];
+//        }
+
+//        private double updateSensor4()
+//        {
+//            return data[3];
+//        }
+
+//        private double updateSensor5()
+//        {
+//            return data[4];
+//        }
+
+//        private double updateSensor6()
+//        {
+//            return data[5];
+//        }
+
+//        private void Form1_Load(object sender, EventArgs e)
+//        {
+
+//        }
+//    }
+
+
+}
 }
